058e17efe854c870cfd9b12940a76850b9a73a33
bhagwan <bhagwangurav15@gmail.com>
2021-06-18 19:50:30 +0530

    fix token issue on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,35 +3,28 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token 
   protected
   def authenticate_request!
-    puts auth_token[:user_id]
-    puts "CCCCC"
-    @current_user ||= User.find(auth_token[:user_id]) if auth_token
-    if(!@current_user)
+    unless user_id_in_token?
       render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+      return
     end
+    @current_user = User.find(auth_token[:user_id])
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
   end
 
   private
   def http_token
-      puts request.headers['Authorization']
-      puts "AAAAA"
       @http_token ||= if request.headers['Authorization'].present?
         request.headers['Authorization'].split(' ').last
       end
-      puts @http_token
-      puts "EEEE"
   end
 
   def auth_token
-    puts http_token
-    puts "BBBBB"
-    
     @auth_token ||= JsonWebToken.decode(http_token)
-    puts @auth_token
-    puts "DDDD"
   end
 
-  #def user_id_in_token?
-  #  http_token && auth_token && auth_token[:user_id].to_i
-  #end
+  def user_id_in_token?
+    puts auth_token
+    http_token && auth_token && auth_token[:user_id].to_i
+  end
 end
