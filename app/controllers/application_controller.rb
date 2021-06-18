e981d8edb0ddc2df437c0a4f4f03e2e2dda1e4df
bhagwan <bhagwangurav15@gmail.com>
2021-06-18 21:05:09 +0530

    fix token issue on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,6 @@ class ApplicationController < ActionController::Base
     @current_user = User.find(auth_token[:user_id])
   rescue JWT::VerificationError, JWT::DecodeError
     render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-  end  
-
-  def user_id_in_token?
-    puts "AAA"
-    puts http_token
-    puts auth_token
-    puts "BBB"
-    http_token && auth_token && auth_token[:user_id].to_i
   end
 
   private
@@ -25,15 +17,13 @@ class ApplicationController < ActionController::Base
       @http_token ||= if request.headers['Authorization'].present?
         request.headers['Authorization'].split(' ').last
       end
-      puts "======="
-      puts @http_token
-      puts "======="
   end
 
   def auth_token
     @auth_token ||= JsonWebToken.decode(http_token)
-    puts "-----------"
-    puts @auth_token
-    puts "----------------"
+  end
+
+  def user_id_in_token?
+    http_token && auth_token && auth_token[:user_id].to_i
   end
 end
