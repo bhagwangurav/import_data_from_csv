87aa4ce95d23bb7dba70d056d16c4780582e6f38
bhagwan <bhagwangurav15@gmail.com>
2021-06-18 18:41:50 +0530

    fix token issue on heroku

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,5 +1,5 @@
 class QuestionController < ApplicationController
-  #before_action :authenticate_request!
+  before_action :authenticate_request!
 
   def index
     #@questions = Question.all.paginate(page: params[:page], per_page: 10).order("#{params[:sort_with]} #{params[:order_by]}")
