498b7842941b8e70bdffc5328859867962825193
bhagwan <bhagwangurav15@gmail.com>
2021-07-09 22:58:11 +0530

    remove comment

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,8 +5,12 @@ class AuthenticationController < ApplicationController
     if @user.valid_password?(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     username: @user.email }, status: :ok
+      render json: { 
+        token: token, 
+        exp: time.strftime("%m-%d-%Y %H:%M"), 
+        username: @user.email 
+      }, 
+      status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
