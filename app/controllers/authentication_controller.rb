2b278efe2f876f5d51ffbc59bf5178e2300a562b
bhagwan <bhagwangurav15@gmail.com>
2021-07-09 23:20:13 +0530

    remove comment

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,6 @@
 class AuthenticationController < ApplicationController
   def authenticate_user
     @user = User.find_for_database_authentication(email: params[:email])
-    puts @user
     if @user.valid_password?(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
@@ -14,11 +13,5 @@ class AuthenticationController < ApplicationController
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
-  end
-
-  private
-
-  def login_params
-    params.permit(:email, :password)
   end
 end
--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,9 +1,8 @@
 class QuestionController < ApplicationController
   before_action :authorize_request
 
+  #Show data
   def index
-    #@questions = Question.all.paginate(page: params[:page], per_page: 10).order("#{params[:sort_with]} #{params[:order_by]}")
-    #@questions = Question.joins(:mapping, :role)
     @questions = Question.select("questions.id, questions.pri, questions.name, teaming_stages.name as teaming_stages_name, questions.appears, questions.frequency, questions.q_type, roles.name as role_name, questions.required, questions.conditions, mappings.name as mapping_name").joins(:mapping, :role, :teaming_stage)
     render json: { "status" => 200, 'response' => @questions}
   end
